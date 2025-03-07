99a49fd75d74c40dc73fb4363b4a0d64e5329016
michaelgreen06 <michaelgreen06@gmail.com>
2025-03-07 13:41:42 +0700

    cleaning up sitemap api directory

--- pages/api/sitemap-locations-13.xml.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { generateLocationSitemap } from '../../lib/utils/sitemap';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await generateLocationSitemap(res, 5);
-}
