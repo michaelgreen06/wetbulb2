9863dbcdae28bb05b22c672c338fdac0fd9b98e6
michaelgreen06 <michaelgreen06@gmail.com>
2025-04-03 17:18:52 +0700

    updated _app.tsx to contain hrefs verification script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from 'next/app';
 import SharedLayout from '../components/layouts/SharedLayout';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SharedLayout>
-      <Component {...pageProps} />
-    </SharedLayout>
+    <>
+      <Head>
+        <script src="https://analytics.ahrefs.com/analytics.js" data-key="3I22XXvbGakzvXR4aAEzfg" async></script>
+      </Head>
+      <SharedLayout>
+        <Component {...pageProps} />
+      </SharedLayout>
+    </>
   );
 }
