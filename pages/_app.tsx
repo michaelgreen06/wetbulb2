d67028fa4c1d8445ec3293206feb88f1faa232b7
michaelgreen06 <michaelgreen06@gmail.com>
2025-04-03 17:27:38 +0700

    moved the script to the _document.tsx which is where it should be since i'm using  the pages router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
 import type { AppProps } from 'next/app';
 import SharedLayout from '../components/layouts/SharedLayout';
-import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <script src="https://analytics.ahrefs.com/analytics.js" data-key="3I22XXvbGakzvXR4aAEzfg" async></script>
-      </Head>
-      <SharedLayout>
-        <Component {...pageProps} />
-      </SharedLayout>
-    </>
+    <SharedLayout>
+      <Component {...pageProps} />
+    </SharedLayout>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <script src="https://analytics.ahrefs.com/analytics.js" data-key="3I22XXvbGakzvXR4aAEzfg" async></script>
       </Head>
       <body>
         <Main />
